d10420e0c765ecbe0a321d3a2ba945c575972e94
rharvier <romain.harvier@gmail.com>
2025-11-01 20:05:12 +0100

    fix: port

--- src/main.rs
+++ src/main.rs
@@ -23,7 +23,7 @@ async fn main() {
         .route("/users", post(create_user));
 
     // run our app with hyper, listening globally on port 3000
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
 
